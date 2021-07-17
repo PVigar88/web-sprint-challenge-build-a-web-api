d1b0f97f3590915029aa9687c0df9abde4ecd909
PVigar88 <Pvigar@gmail.com>
2021-07-16 22:59:29 -0400

    started Action routers

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,22 @@
 // Write your "actions" router here!
+const express = require("express");
+
+const Actions = require("../actions/actions-model.js");
+
+const router = express.Router();
+
+router.use(express.json());
+
+router.get("/", (req, res) => {
+  Actions.get()
+    .then((actions) => {
+      res.status(200).json(actions);
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({
+          message: "A Server Error has occured (Error Code: pv-actrtr-gt)",
+        });
+    });
+});
