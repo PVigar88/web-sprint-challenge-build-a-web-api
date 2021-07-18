7ef0b309b7e1b432de99a304f8764f7969f215fa
PVigar88 <Pvigar@gmail.com>
2021-07-18 14:15:27 -0400

    20 tests passing

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -55,21 +55,69 @@ router.post("/", (req, res) => {
 });
 
 router.put("/:id", (req, res) => {
-  Projects.update(id, changes)
-    .then((updatedAction) => {})
-    .catch((err) => {
-      res.status(500).json({
-        message: "A Server Error has occured (Error Code: pjtrtr-pt)",
-      });
+  if (!req.body.name || !req.body.description || !req.body.completed) {
+    res.status(400).json({
+      message: "Project must have name, decription and completed status",
     });
+  } else {
+    const changes = req.body;
+    const id = req.params.id;
+    Projects.update(id, { changes })
+      .then((updatedProject) => {
+        if (updatedProject) {
+          return Projects.get(req.params.id);
+        } else {
+          res.status(404).json({ message: "project Id not found" });
+        }
+      })
+      .then((updatedProject) => {
+        res.status(200).json(updatedProject);
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: "A Server Error has occured (Error Code: pjtrtr-pt)",
+        });
+      });
+  }
 });
 router.delete("/:id", (req, res) => {
-  Projects.remove(id)
-    .then((count) => {})
+  Projects.get(req.params.id).then((project) => {
+    if (project) {
+      Projects.remove(id)
+        .then((count) => {
+          if (count) {
+            res.status(200).end();
+          } else {
+            res.status(404).json({
+              message: "The project with the specified ID does not exist",
+            });
+          }
+        })
+        .catch((err) => {
+          res.status(500).json({
+            message: "A Server Error has occured (Error Code: pjtrtr-dlt)",
+          });
+        });
+    } else {
+      res
+        .status(404)
+        .json({ message: "There is no project witht he given id" });
+    }
+  });
+});
+
+router.get("/:id/actions", (req, res) => {
+  Projects.getProjectActions(req.params.id)
+    .then((actions) => {
+      if (actions) {
+        res.status(200).json(actions);
+      } else {
+        res.status(404).json({ message: "no project with provided id" });
+      }
+    })
     .catch((err) => {
-      res.status(500).json({
-        message: "A Server Error has occured (Error Code: pjtrtr-dlt)",
-      });
+      res.status(500).json({ message: "a server error has occured" });
     });
 });
+
 module.exports = router;
